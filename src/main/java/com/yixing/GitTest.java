34ca0d4c1c10e4e0ba277f5fa6249da1146d3d16
ASUS <Layne@atguigu.com>
2023-03-04 09:21:46 +0800

    si commit

--- src/main/java/com/yixing/GitTest.java
+++ src/main/java/com/yixing/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello hello hello");
         System.out.println("hello2 hello2 hello2");
+        System.out.println("hello3 hello3 hello3");
 
 
     }
