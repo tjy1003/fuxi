140ba04a7ff3bc692af554a092b1db9921c158bd
ASUS <Layne@atguigu.com>
2023-03-04 09:24:35 +0800

    master commit

--- src/main/java/com/yixing/GitTest.java
+++ src/main/java/com/yixing/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello2 hello2 hello2");
         System.out.println("hello3 hello3 hello3");
         System.out.println("hot-fix commit");
+        System.out.println("master commit");
+
 
 
     }
