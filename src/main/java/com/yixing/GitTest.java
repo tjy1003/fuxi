67f214424ac885ed921ec43cff5bfec576eaac50
ASUS <Layne@atguigu.com>
2023-03-04 10:16:55 +0800

    push github master

--- src/main/java/com/yixing/GitTest.java
+++ src/main/java/com/yixing/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hot-fix commit");
         System.out.println("master commit");
         System.out.println("hot-fix commit2");
+        System.out.println("push github commit");
 
 
     }
